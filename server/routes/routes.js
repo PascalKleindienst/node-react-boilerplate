8dc72f9755e6a528b5ccf9c600c11edc0ca17d78
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-02-20 21:02:31 +0100

    fixed missing path import

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,7 +3,10 @@
  * @param  {Object} app        The Server
  * @param  {String} publicPath Path to public folder
  */
+
 module.exports = (app, publicPath) => {
+    const path = require('path');
+
     app.get('*', (req, res) => {
         res.sendFile(path.join(publicPath, 'index.html'));
     });
