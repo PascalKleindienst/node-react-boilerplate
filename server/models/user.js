1afc6e0ab9e05dfc36ce4e95e5956e0e1e07e490
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-02-21 10:09:21 +0100

    added user model

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: {
+        type: DataTypes.STRING,
+        validate: {
+            isEmail: true
+        }
+    },
+    name: DataTypes.STRING,
+    token: DataTypes.STRING,
+    provider: DataTypes.ENUM('twitter', 'google', 'facebook', 'github'),
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  }, {});
+
+  User.associate = (models) => {
+    // associations can be defined here
+  };
+
+  return User;
+};
