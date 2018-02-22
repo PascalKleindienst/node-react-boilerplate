14bac85ec8117fd0ff279212f8b59ba35b9f0cae
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-02-22 09:37:10 +0100

    added a loading page component

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,12 @@
+// ==============================================
+// LOADING PAGE COMPONENT =======================
+// ==============================================
+import React from 'react';
+
+const LoadingPage = () => (
+    <div className="loader">
+        <img className="loader__image" src="/images/loader.svg" />
+    </div>
+);
+
+export default LoadingPage;
