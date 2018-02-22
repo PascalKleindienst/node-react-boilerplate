ac22c882ddba637ff6c231bedffd7ebf1eda1f0c
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-02-22 11:48:38 +0100

    added a profile picture to the header bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,22 +7,40 @@ import { connect } from 'react-redux';
 import { logout } from '../actions/auth';
 
 // Header Component
-export const Header = ({ logout }) => (
+export const Header = ({ auth, logout }) => (
     <header className="header">
         <div className="content-container">
             <div className="header__content">
                 <Link className="header__title" to="/dashboard">
                     <h1>Boilerplate</h1>
                 </Link>
-                <button className="button button--link" onClick={logout}>Logout</button>
+                <div className="profile has-dropdown">
+                    <img src={ auth.picture } alt="" className="profile__picture" />
+                    <div className="dropdown-menu">
+                        <div className="dropdown-menu__header">
+                            <strong>Account</strong>
+                        </div>
+                        <Link className="dropdown-menu__item" to="/profile">
+                            Profile
+                        </Link>
+                        <Link className="dropdown-menu__item" onClick={logout} to="#">
+                            Logout
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     </header>
 );
+
+// States
+const mapStateToProps = (state) => ({
+    auth: state.auth
+});
 
 // Dispatch Functions
 const mapDispatchToProps = (dispatch) => ({
     logout: () => dispatch(logout())
 });
 
-export default connect(undefined, mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
