788692b8de23662ec6043fea0bf4fe5cee9fcdad
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-02-22 09:30:21 +0100

    added not found page component

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,13 @@
+// ==============================================
+// NOT FOUND PAGE COMPONENT =====================
+// ==============================================
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => (
+    <div>
+        404! - <Link to="/">Go home, you're drunk!</Link>
+    </div>
+);
+
+export default NotFoundPage;
