26ad4f3f62422823e5d896f71f56158f97a5cedc
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-02-22 09:38:11 +0100

    added a dashboard page

---
+++ src/components/DashboardPage.js
@@ -0,0 +1,12 @@
+// ==============================================
+// DASHBOARD PAGE COMPONENT =====================
+// ==============================================
+import React from 'react';
+
+const DashboardPage = () => (
+    <div>
+        Dashboard page content
+    </div>
+);
+
+export default DashboardPage;
