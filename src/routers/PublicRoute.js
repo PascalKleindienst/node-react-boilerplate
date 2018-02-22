3b5f1de6f37ac69fd1774df936c3e60fd3e8ea3e
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-02-22 09:39:13 +0100

    added router

---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,28 @@
+// ==============================================
+// PUBLIC ROUTE =================================
+// ==============================================
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+
+// Public Route Component
+export const PublicRoute = ({
+    isAuthenticated,
+    component: Component,
+    ...rest
+}) => (
+    <Route {...rest} component={(props) => (
+        isAuthenticated ? (
+            <Redirect to="/dashboard" />
+        ) : (
+            <Component {...props} />
+        )
+    )} />
+);
+
+// States
+const mapStateToProps = (state) => ({
+    isAuthenticated: !!state.auth.uid
+});
+
+export default connect(mapStateToProps)(PublicRoute);
