c53be7240bcb90be8f9ce687b101f91b59b11999
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-02-24 16:46:08 +0100

    test more generic / non existing url

--- tests/server/routes/routes.test.js
+++ tests/server/routes/routes.test.js
@@ -5,8 +5,8 @@ const request = require('supertest');
 const app = require('../../../server/app');
 
 describe('my routes', () => {
-    test('should return index.html on GET', () => {
-        return request(app).get('/').then(response => {
+    test('should return index.html on default GET request', () => {
+        return request(app).get('/some/foo/bar').then(response => {
             expect(response.statusCode).toBe(200);
             expect(response.type).toMatch(/html/);
             expect(response.text).toMatchSnapshot();
